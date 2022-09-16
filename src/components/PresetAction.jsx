9b0edd597253e12574792f3993cac9ce1ba802d6
Patrick Desmarais <patrick.desm@gmail.com>
2022-09-16 14:45:39 -0400

    Add units to preset actions' total minutes

--- src/components/PresetAction.jsx
+++ src/components/PresetAction.jsx
@@ -4,7 +4,7 @@ export default function PresetAction({ totalMinutes, title }) {
     return (
         <div className="presets-action">
             <button>
-                <div className="presets-action-minutes">{totalMinutes}</div>
+                <div className="presets-action-minutes">{totalMinutes + "m"}</div>
                 <div className="presets-action-title">{title}</div>
             </button>
         </div>
