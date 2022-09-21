a2ef7f4ed9c806f6ad3dd57169345dc3298c0644
Patrick Desmarais <patrick.desm@gmail.com>
2022-09-21 13:50:17 -0400

    Format code for readability

--- src/components/TimeDisplay.jsx
+++ src/components/TimeDisplay.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
 export default function TimeDisplay(props) {
-    const downIndicatorClassName = (props.timerState === "running" ? "down-indicator down-indicator-active" : "down-indicator");
-    const downIndicatorSrc = (props.timerState === "running" ? props.icons["DownIndicator"]["Active"] : props.icons["DownIndicator"]["Inactive"]);
+    const downIndicatorClassName = (
+        props.timerState === "running" ?
+            "down-indicator down-indicator-active" :
+            "down-indicator"
+    );
+    const downIndicatorSrc = (
+        props.timerState === "running" ?
+            props.icons["DownIndicator"]["Active"] :
+            props.icons["DownIndicator"]["Inactive"]
+    );
 
-    const formattedMinutesLeft = props.minutesLeft > 0 ? props.minutesLeft + "m" : "0m";
+    const formattedMinutesLeft = (
+        props.minutesLeft > 0 ?
+            props.minutesLeft + "m" :
+            "0m"
+    );
 
     return (
         <div className="time-display">
