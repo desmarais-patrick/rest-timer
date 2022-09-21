6fdd46f544a1ba879a4e4f67ab42d2c5d3f0a84d
Patrick Desmarais <patrick.desm@gmail.com>
2022-09-21 14:47:48 -0400

    Fix issue with settings input
    - Erasing all values with keyboard isn't blocked

--- src/components/SettingsPresetItem.jsx
+++ src/components/SettingsPresetItem.jsx
@@ -13,30 +13,45 @@ const titleInputStyle = {
     width: "8rem",
 };
 
+function parseMinutes(inputValue) {
+    if (typeof inputValue === "number" && isNaN(inputValue) === false) {
+        return inputValue;
+    }
+
+    if (typeof inputValue !== "string" || inputValue.length === 0) {
+        return 1;
+    }
+
+    try {
+        const parsedValue = parseInt(inputValue);
+        if (isNaN(parsedValue) === false) {
+            return parsedValue;
+        }
+    } catch (e) {
+        // Silent parsing failed.
+    }
+
+    return 1;
+}
+
 export default function SettingsPresetItem({ presetId, translations, initialMinutes, initialTitle, onChange }) {
     const [minutes, setMinutes] = useState(initialMinutes);
     const [title, setTitle] = useState(initialTitle);
 
     const onMinuteChange = (event) => {
-        let intValue = event.target.value;
-        if (typeof intValue === "string") {
-            try {
-                intValue = parseInt(intValue);
-            } catch (e) {
-                intValue = 1;
-            }
-        }
-        setMinutes(intValue);
+        setMinutes(event.target.value);
     };
     const onTitleChange = (event) => {
         setTitle(event.target.value);
     };
 
     const onBlur = () => {
-        onChange(presetId, minutes, title);
+        const parsedMinutes = parseMinutes(minutes);
+        onChange(presetId, parsedMinutes, title);
     };
 
-    const unitsMinuteLabel = minutes > 1 ?
+    const parsedMinutes = parseMinutes(minutes);
+    const unitsMinuteLabel = parsedMinutes > 1 ?
         translations["unitsMinutePlural"] :
         translations["unitsMinuteSingular"];
 
